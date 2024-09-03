50ff673a24ec70ed206e15bc5c152fd60015d8bb
Usuario <Usuario@CPD02-M15>
2024-09-03 11:36:35 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroAlunos/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace CadastroAlunos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCurso = new System.Windows.Forms.TextBox();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.dgvAlunos = new System.Windows.Forms.DataGridView();
+            this.txtAlteracao = new System.Windows.Forms.TextBox();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnTodos = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Curso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(266, 67);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(237, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtCurso
+            // 
+            this.txtCurso.Location = new System.Drawing.Point(266, 108);
+            this.txtCurso.Name = "txtCurso";
+            this.txtCurso.Size = new System.Drawing.Size(237, 20);
+            this.txtCurso.TabIndex = 1;
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(266, 170);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(106, 35);
+            this.btnIncluir.TabIndex = 2;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(399, 170);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(104, 35);
+            this.btnRemover.TabIndex = 3;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // dgvAlunos
+            // 
+            this.dgvAlunos.AllowUserToAddRows = false;
+            this.dgvAlunos.AllowUserToDeleteRows = false;
+            this.dgvAlunos.AllowUserToOrderColumns = true;
+            this.dgvAlunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlunos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nome,
+            this.Curso});
+            this.dgvAlunos.Location = new System.Drawing.Point(176, 238);
+            this.dgvAlunos.Name = "dgvAlunos";
+            this.dgvAlunos.ReadOnly = true;
+            this.dgvAlunos.Size = new System.Drawing.Size(377, 159);
+            this.dgvAlunos.TabIndex = 4;
+            this.dgvAlunos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAlunos_CellClick);
+            // 
+            // txtAlteracao
+            // 
+            this.txtAlteracao.Location = new System.Drawing.Point(250, 428);
+            this.txtAlteracao.Name = "txtAlteracao";
+            this.txtAlteracao.Size = new System.Drawing.Size(237, 20);
+            this.txtAlteracao.TabIndex = 5;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(493, 425);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(60, 23);
+            this.btnAlterar.TabIndex = 6;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnTodos
+            // 
+            this.btnTodos.Location = new System.Drawing.Point(372, 489);
+            this.btnTodos.Name = "btnTodos";
+            this.btnTodos.Size = new System.Drawing.Size(90, 36);
+            this.btnTodos.TabIndex = 7;
+            this.btnTodos.Text = "Excluir Todos";
+            this.btnTodos.UseVisualStyleBackColor = true;
+            this.btnTodos.Click += new System.EventHandler(this.btnTodos_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(468, 489);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(85, 36);
+            this.btnFechar.TabIndex = 8;
+            this.btnFechar.Text = "Fechar Sistema";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Location = new System.Drawing.Point(337, 501);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(19, 22);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Curso:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 435);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Alterar Nome:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(176, 501);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Total de Alunos:";
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Curso
+            // 
+            this.Curso.HeaderText = "Curso";
+            this.Curso.Name = "Curso";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 682);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnTodos);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.txtAlteracao);
+            this.Controls.Add(this.dgvAlunos);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.txtCurso);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Cadastro de Alunos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCurso;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.DataGridView dgvAlunos;
+        private System.Windows.Forms.TextBox txtAlteracao;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnTodos;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Curso;
+    }
+}
+
---
+++ CadastroAlunos/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroAlunos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            dgvAlunos.Rows.Add(txtNome.Text, txtCurso.Text);
+            txtNome.Clear();
+            txtCurso.Clear();
+
+            MessageBox.Show("Aluno incluído com sucesso", "Inclusão", MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+
+            lblTotal.Text = dgvAlunos.RowCount.ToString();
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (dgvAlunos.Rows.Count > 0)
+            {
+                dgvAlunos.Rows.RemoveAt(dgvAlunos.CurrentRow.Index);
+                MessageBox.Show("Aluno Excluído com sucesso", "Exclusão", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                lblTotal.Text = dgvAlunos.RowCount.ToString();
+            
+            }
+
+        }
+
+        private void dgvAlunos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvAlunos.RowCount > 0)
+            {
+                txtAlteracao.Text = dgvAlunos.CurrentRow.Cells[0].Value.ToString();
+            }
+
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (txtAlteracao.Text != "")
+            {
+                dgvAlunos.CurrentRow.Cells[0].Value = txtAlteracao.Text;
+                MessageBox.Show("Aluno alterado com sucesso", "Alterar", MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnTodos_Click(object sender, EventArgs e)
+        {
+            dgvAlunos.RowCount = 0;
+            lblTotal.Text = dgvAlunos.RowCount.ToString();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
